91ba6cce9a81d3e7b06031aad600feb8802ea41d
maritza <maritza.bello@islas.org.mx>
2021-08-12 10:05:17 -0700

    🧪 add failing tests

--- tests/testthat/test_nothing.R
+++ tests/testthat/test_nothing.R
@@ -1,4 +1,5 @@
 library(testthat)
+library(tidyverse)
 source('../../R/dateros.R')
 
 describe("Test dummy", {
@@ -6,5 +7,15 @@ describe("Test dummy", {
     expected <- 1
     obtained <- return_1()
     expect_equal(expected,obtained,tolerance = 1e-3)
+  })
+})
+
+describe("Divide datos proporción 20/80", {
+  datos_prueba <- tibble(A=c(1,2,3,4,5),B=c('A','B','C','D','E'))
+  it("Que los renglones sean 4", {
+    expected_n_row <- 4
+    obtained_data <- divide_data(datos_prueba)
+    obtained_n_row <- nrow(obtained_data)
+    expect_equal(expected_n_row,obtained_n_row,tolerance = 1e-3)
   })
 })
