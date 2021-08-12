d250a1990e21ddc9282474d3dddd3b5665d3f615
maritza <maritza.bello@islas.org.mx>
2021-08-12 11:09:11 -0700

    🧪 Add failing tests

--- tests/testthat/test_nothing.R
+++ tests/testthat/test_nothing.R
@@ -33,3 +33,24 @@ describe("Divide data in a proportion of 80/20", {
     expect_equal(expected_n_row,obtained_n_row,tolerance = 1e-3)
   })
 })
+
+describe("Test fit mean", {
+  datos_prueba <- tibble(A=c(1,2,3,4,5),B=c('A','B','C','D','E'),target=c(3,3,3,3,3),id=c('A','B','C','D','E'))
+  it("Mean is correct", {
+    expected <- 3
+    obtained <- fit_mean(datos_prueba)
+    expect_equal(expected,obtained,tolerance = 1e-3)
+  })
+  datos_prueba <- tibble(A=c(1,2,3,4,5),B=c('A','B','C','D','E'),target=c(6,4,5,5,5),id=c('A','B','C','D','E'))
+  it("Mean is correct", {
+    expected <- 5
+    obtained <- fit_mean(datos_prueba)
+    expect_equal(expected,obtained,tolerance = 1e-3)
+  })
+  datos_prueba <- tibble(A=c(1,2,3,4,5),B=c('A','B','C','D','E'),target=c(6,4,8,5,2),id=c('A','B','C','D','E'))
+  it("Mean is correct", {
+    expected <- 5
+    obtained <- fit_mean(datos_prueba)
+    expect_equal(expected,obtained,tolerance = 1e-3)
+  })
+})
