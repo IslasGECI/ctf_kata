cbbd18d38ddeb6735c90f2a47f77479bf85aee90
maritza <maritza.bello@islas.org.mx>
2021-08-12 11:10:15 -0700

    ✅ Pass test, implement function to calculate mean

--- R/dateros.R
+++ R/dateros.R
@@ -6,3 +6,8 @@ divide_data <- function(input_data){
     output_data <- sample_frac(input_data,size=0.8)
     return(output_data)
 }
+
+fit_mean <- function(input_data){
+    mean_target <- mean(input_data$target)
+    return(mean_target)
+}
