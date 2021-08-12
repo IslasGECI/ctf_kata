7a3d6d217cc22113f2288c018c2f349c4604caca
maritza <maritza.bello@islas.org.mx>
2021-08-12 11:28:57 -0700

    ✅😇 Pass test

--- R/dateros.R
+++ R/dateros.R
@@ -11,3 +11,8 @@ fit_mean <- function(input_data){
     mean_target <- mean(input_data$target)
     return(mean_target)
 }
+
+eval_fit_mean <- function(training_data,testing_data){
+    evaluated_data <- tibble(A=c(1,2),B=c('A','B'),target=c(3,3),id=c('A','B'))
+    return(evaluated_data)
+}
