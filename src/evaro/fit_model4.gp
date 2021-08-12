5ad24b7bb173ae532bf5fff3530f6efffab1ec8e
Evaristo Rojas <evaristo.rojas@islas.org.mx>
2021-08-12 15:31:02 -0700

    🧑‍💻 Use weight instead of tarsus

--- src/evaro/fit_model4.gp
+++ src/evaro/fit_model4.gp
@@ -3,4 +3,4 @@
 set datafile separator ","
 set fit logfile "fit4.log"
 g(x) = a*x**2 + b*x + c
-fit g(x) 'pollos_petrel/train2.csv' using 1:2 via a, b, c
+fit g(x) 'pollos_petrel/train4.csv' using 1:2 via a, b, c
--- src/evaro/get_submission4.sh
+++ src/evaro/get_submission4.sh
@@ -4,6 +4,6 @@ a=$(cat a.txt)
 b=$(cat b.txt)
 c=$(cat c.txt)
 sed "s/NA/0/g" pollos_petrel/test.csv > pollos_petrel/test4.csv
-csvsql --query "select id,(${a}*Longitud_tarso*Longitud_tarso+${b}*Longitud_tarso+${c}) as 'target' from 'test4'" pollos_petrel/test4.csv > pollos_petrel/tmp_submission.csv
+csvsql --query "select id,(${a}*Peso*Peso+${b}*Peso+${c}) as 'target' from 'test4'" pollos_petrel/test4.csv > pollos_petrel/tmp_submission.csv
 mean_target=$(csvstat pollos_petrel/train.csv | grep Mean | tail -1 | grep --extended-regexp --only-matching --regexp=[0-9]+\.[0-9]+)
 sed "s/${c}/${mean_target}/g" pollos_petrel/tmp_submission.csv
