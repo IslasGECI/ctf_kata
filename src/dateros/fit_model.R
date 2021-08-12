1cae55551dc421f1fa8d2fec42a20d48ccf70a59
maritza <maritza.bello@islas.org.mx>
2021-08-12 12:35:49 -0700

    💄 Apply format

--- src/dateros/fit_model.R
+++ src/dateros/fit_model.R
@@ -1,12 +1,12 @@
 library(tidyverse)
-source('R/dateros.R')
+source("R/dateros.R")
 
 original_data <- read_csv("pollos_petrel/train.csv")
 testing_data <- read_csv("pollos_petrel/test.csv")
 
 training_data <- divide_data(original_data)
 
-submission_data <- eval_fit_mean(training_data,testing_data) %>%
-  select(c(id,target))
+submission_data <- eval_fit_mean(training_data, testing_data) %>%
+  select(c(id, target))
 
-write_csv(submission_data,"pollos_petrel/dateros_submission.csv")
+write_csv(submission_data, "pollos_petrel/dateros_submission.csv")
